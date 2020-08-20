3d413334977ec6f164f6299a7066d8e1c57615a8
Raymond Nijssen <r.nijssen@terglobo.nl>
2020-08-20 19:30:17 +0200

    fix for infinite download loop

--- traffic_signs.py
+++ traffic_signs.py
@@ -13,14 +13,20 @@ status_code = 200
 # start a offset
 # default: '1970-01-01T00:00:00.000Z'
 offset = '1970-01-01T00:00:00.000Z'
+#offset = '2020-08-01T12:00:00.000Z'
 
-while (status_code == 200):
+last_offset = ''
+object_count = 0
+
+while (status_code == 200 and offset != last_offset):
+    last_offset = offset
     url = 'https://data.ndw.nu/api/rest/static-road-data/traffic-signs/v1/events?offset={}&limit=100'.format(offset)
     response = requests.get(url, verify=False)
     status_code = response.status_code
-    print ("{} Status:{}".format(url,status_code))
+    print( "{} Status:{} Objects:{}".format(url, status_code, object_count) )
     if (status_code == 200):
         dataJSON = response.json()
+        object_count += len(dataJSON)
         if (pdObjNorm is None):
             pdObjNorm = json_normalize(dataJSON)
         else:
