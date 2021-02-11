e04d4ec92aaea4bdcef7f3ebf9d532f4273764a8
Antoon Uijtdehaag <Uijtdehaag@users.noreply.github.com>
2021-02-11 19:54:32 +0100

    Update traffic_signs.py

--- traffic_signs.py
+++ traffic_signs.py
@@ -2,24 +2,43 @@
 # https://docs.ndw.nu/api/trafficsigns/en/index.html 
 #
 # call api and save data to csv file
-
-import requests, json
+import requests, json, os
 import pandas as pd
 from pandas.io.json import json_normalize
+
+def GetLastOffset(filename):
+        
+    # Open ID file
+    if os.path.isfile(filename):
+        idFile = open(filename, "r")    
+        Offset = idFile.read()        
+        idFile.close()
+    else:
+        # default: '1970-01-01T00:00:00.000Z'
+        Offset = '1970-01-01T00:00:00.000Z'
+        
+    return Offset
+
+def SaveLastOffset( filename, Offset):    
+
+    idFile = open(filename, "w")    
+    idFile.write("{}".format(Offset))
+    idFile.close()
+
 
 pdObjNorm = None
 status_code = 200
 
-# start a offset
-# default: '1970-01-01T00:00:00.000Z'
-offset = '1970-01-01T00:00:00.000Z'
-#offset = '2020-08-01T12:00:00.000Z'
+exportFileName = "traffic_signs.csv"
+StoreOffSetFilename = "offset.txt"
+offset = GetLastOffset(StoreOffSetFilename)
 
 last_offset = ''
 object_count = 0
 
 while (status_code == 200 and offset != last_offset):
     last_offset = offset
+    
     url = 'https://data.ndw.nu/api/rest/static-road-data/traffic-signs/v1/events?offset={}&limit=100'.format(offset)
     response = requests.get(url, verify=False)
     status_code = response.status_code
@@ -32,7 +51,12 @@ while (status_code == 200 and offset != last_offset):
         else:
             pdObjNorm = pdObjNorm.append(json_normalize(dataJSON))
         offset = pdObjNorm['publication_timestamp'].max()
-        
 
-#df_duplicates_removed = pdObjNorm.drop_duplicates()
-pdObjNorm.to_csv("traffic_signs.csv",index=False,sep=';')
+        SaveLastOffset(StoreOffSetFilename, offset)
+
+        if os.path.isfile(exportFileName):
+            pdObjNorm.to_csv(exportFileName,index=False,sep=';', mode='a', header=False)
+        else:
+            pdObjNorm.to_csv(exportFileName,index=False,sep=';', mode='w', header=True)
+
+
